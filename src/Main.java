25ba4f503194c7b45c20070c0db38742ff8be47a
Ulyana0830 <Uulyanashmakovaa@gmail.com>
2024-10-09 15:25:32 +0300

    LAB1.2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Stock sber = new Stock("SBER", "ПАО Сбербанк"); sber.setPreviousClosingPrice(281.50); sber.setCurrentPrice(282.87);
+        System.out.println("Код: " + sber.getSymbol()); System.out.println("Имя: " + sber.getName()); System.out.println("Стоимость акций: " +
+                sber.getCurrentPrice());
+        System.out.printf("Процент изменения: %.2f%%", sber.getChangePercent());
     }
 }
