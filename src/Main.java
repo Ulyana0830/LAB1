f5d80617a8eef4b4e847c702bb83ac445f7111bf
Ulyana0830 <Uulyanashmakovaa@gmail.com>
2024-10-09 15:27:31 +0300

    LAB1.3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.text.SimpleDateFormat; import java.util.Date;
+import java.util.Locale;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+    public static void main(String[] args) { long[] timeValuesInSeconds = {
+            10000L,
+            100000L,
+            1000000L,
+            10000000L,
+            100000000L,
+            1000000000L,
+            10000000000L,
+            100000000000L
+    };
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy HH:mm:ss", new Locale("ru", "RU"));
+
+        for (long timeInSeconds : timeValuesInSeconds)
+        {
+            long timeInMillis = timeInSeconds * 1000; Date date = new Date(timeInMillis);
+            String formattedDate = dateFormat.format(date);
+
+            System.out.println("Время: " + timeInSeconds + " секунд -> Дата: " + formattedDate);
         }
     }
 }
